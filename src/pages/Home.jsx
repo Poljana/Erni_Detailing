9fcea3d197b0a7bfe876fb1a87ebafb767829bb2
Luccci <lucijan.pavin@gmail.com>
2025-04-25 20:27:58 +0200

    Landing page napreduje

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import HomeCSS from "..\\css\\home.module.css"
 import { Link } from "react-router-dom"
+import Card1 from "../components/card1"
 
 function Home() {
 
@@ -8,6 +9,18 @@ function Home() {
             <div className={HomeCSS.kartica_br}>
                 <p className={HomeCSS.broj}>{broj}</p>
                 <p className={HomeCSS.tekst}>{tekst}</p>
+            </div>
+        )
+    }
+
+    const Odlike = ({img, title, opis}) => {
+        return(
+            <div className={HomeCSS.odlika}>
+                <div className={HomeCSS.subtitle}>
+                    <img src={img} alt="image" />
+                    <p>{title}</p>
+                </div>
+                <p>{opis}</p>
             </div>
         )
     }
@@ -41,8 +54,45 @@ function Home() {
                 <KarticaBroj broj="03" tekst="Brzi PROCES" />
                 <KarticaBroj broj="04" tekst="Izuzetno POVOLJNO" />
             </div>
-            <div></div>
-            <div></div>
+            <h2>Naše Usluge</h2>
+            <div className={HomeCSS.nase_usluge}>                
+                 <Card1 
+                 title="Detailing vozila"
+                 desc="Priuštite svom vozilu temeljito ručno 
+                 pranje i profesionalnu zaštitu površine"
+                />
+                <Card1 
+                 title="Detailing brodova"
+                 desc="Osigurajte svojoj jahti ili brodu 
+                 vrhunski tretman koji zaslužuje."
+                />
+            </div>
+            <div className={HomeCSS.odlike}>                
+                <div className={HomeCSS.kartice}>
+                    <h2>Zašto izabrati Nas?</h2>
+                    <Odlike 
+                     img="src\assets\Precision.png" 
+                     title="Precizan rad"
+                     opis="Održavamo najviše standarde
+                     profesionalnosti pri detailingu vaših vozila i plovila."
+                    />
+                    <Odlike 
+                     img="src\assets\Diamond.png" 
+                     title="Premium proizvodi i usluge"
+                     opis="Osigurajte dugovječnost vaše
+                     imovine s periodičnim tretmanom detailinga
+                     i zaštite svih površina."
+                    />
+                    <Odlike 
+                     img="src\assets\Security.png" 
+                     title="Visoka razina sigurnosti i privatnosti"
+                     opis="Razumijemo važnost privatnosti i sigurnosti
+                     za klijente koji nam povjeravaju svoja
+                     luksuzna vozila i plovila."
+                    />
+                </div>
+                <img src="src\assets\image2.png" alt="image2" />
+            </div>
             <div></div>
             <div></div>
             <div></div>
